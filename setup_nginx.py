fd58c12b84dc0b9532b7503a8afe6a7300890fbc
Raphaël Vinot <raphael@vinot.info>
2020-03-12 13:48:04 +0100

    chg: Add optional home-made nginx.tmpl

--- setup_nginx.py
+++ setup_nginx.py
@@ -18,8 +18,14 @@ if not docker_content['services']['nginx-proxy'].get('networks'):
     docker_content['services']['nginx-proxy']['networks'] = ['default', 'misp-test-sync']
     docker_content['networks'] = {'misp-test-sync': {'external': {'name': internal_network_name}}}
 
+if '443:443' not in docker_content['services']['nginx-proxy']['ports']:
+    docker_content['services']['nginx-proxy']['ports'].append('443:443')
+
 if Path('certs').exists() and '../certs:/etc/nginx/certs' not in docker_content['services']['nginx-proxy']['volumes']:
     docker_content['services']['nginx-proxy']['volumes'].append('../certs:/etc/nginx/certs')
+
+if Path('nginx.tmpl').exists() and '../nginx.tmpl:/app/nginx.tmpl' not in docker_content['services']['nginx-proxy']['volumes']:
+    docker_content['services']['nginx-proxy']['volumes'].append('../nginx.tmpl:/app/nginx.tmpl')
 
 with docker_compose_file.open('w') as f:
     f.write(yaml.dump(docker_content, default_flow_style=False))
