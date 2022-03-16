9fbc18de75ed64e8e4c6fec8ce4c30b3f04c8e57
Raphaël Vinot <raphael@vinot.info>
2022-03-16 10:14:18 +0100

    new: config parser

---
+++ aggregate_auth_no_create.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import csv
+import json
+from pathlib import Path
+
+
+def auth_from_config(config):
+    auth_admin = {'url': config['baseurl'], 'login': 'admin@admin.test', 'authkey': config['admin_key'], 'password': config['admin_password']}
+    site_admin = {'url': config['baseurl'], 'login': config['email_site_admin'], 'authkey': config['site_admin_authkey'], 'password': config['site_admin_password']}
+    org_admin = {'url': config['baseurl'], 'login': config['email_orgadmin'], 'authkey': config['orgadmin_authkey'], 'password': config['orgadmin_password']}
+    return auth_admin, site_admin, org_admin
+
+
+if __name__ == '__main__':
+    to_dump = []
+    for cp in Path('misps').glob('**/config.json'):
+        with cp.open() as f:
+            config = json.load(f)
+        auth_admin, site_admin, org_admin = auth_from_config(config)
+        to_dump.append(auth_admin)
+        to_dump.append(site_admin)
+        to_dump.append(org_admin)
+
+    with (Path('misps') / 'auth.json').open('w') as f:
+        json.dump(to_dump, f, indent=2)
+
+    with (Path('misps') / 'auth.csv').open('w') as csvfile:
+        fieldnames = ['url', 'login', 'authkey', 'password']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for a in to_dump:
+            writer.writerow(a)
