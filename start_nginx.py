a6db23adb91bd8bcdffb9a2120d570ce1854a960
Raphaël Vinot <raphael@vinot.info>
2024-02-06 14:38:48 +0100

    fix: Missing docker compose v2

--- start_nginx.py
+++ start_nginx.py
@@ -9,7 +9,7 @@ from pathlib import Path
 nginx_root = Path('nginx-proxy')
 cur_dir = os.getcwd()
 os.chdir(nginx_root)
-command = shlex.split('sudo docker-compose up -d')
+command = shlex.split('sudo docker compose up -d')
 p = Popen(command)
 p.wait()
 os.chdir(cur_dir)
