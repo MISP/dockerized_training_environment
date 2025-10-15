94244b672cfd1b3e1097f4600ab88e0b889c464d
Sami Mokaddem <mokaddem.sami@gmail.com>
2025-10-15 11:44:43 +0200

    new: [config] Added config entries to add more orgs and sgs

--- generic_config.py
+++ generic_config.py
@@ -15,6 +15,23 @@ central_node_name = 'misp-central'
 prefix_client_node = 'misp-'
 hostname_suffix = '.local'
 
+instances_number_width = len(str(number_instances))
+client_node_name_getter = None
+# client_node_name_getter = (
+#     lambda instance_id: f"{prefix_client_node}{instance_id:0{instances_number_width}}"
+# )
+# mapping = {
+#     0: 'NATO-MN-MISP',
+#     1: 'NATO',
+#     2: 'ALB',
+#     3: 'BEL',
+#     4: 'BGR',
+#     5: 'XYZ',
+# }
+# client_node_name_getter = (
+#     lambda instance_id: f"{prefix_client_node}{mapping[instance_id]}"
+# )
+
 # #### Sync config
 
 secure_connection = False
@@ -22,6 +39,12 @@ central_node_org_name = 'Central Node'
 client_node_org_name_prefix = 'Node '
 admin_email_name = 'admin'
 orgadmin_email_name = 'orgadmin'
+client_node_org_name_getter = None
+# client_node_org_name_getter = lambda instance_id: f'{client_node_org_name_prefix}{instance_id:0{instances_number_width}}'
+# client_node_org_name_getter = (
+#     lambda instance_id: f"{client_node_org_name_prefix}{mapping[instance_id]}"
+# )
+
 
 tag_central_to_nodes = ['push_to_nodes', 'push_to_nodes_alt']
 tag_nodes_to_central = ['push_to_central', 'push_to_central_alt']
@@ -42,3 +65,35 @@ local_tags_central = ['push_to_nodes']
 reserved_tags_central = ['push_to_nodes_alt']
 # Tags reserved for the clients
 local_tags_clients = ['push_to_central_alt']
+
+
+additional_orgs_per_instance = None
+# additional_orgs_per_instance = {
+#     1: [],
+#     2: [],
+#     3: ["CERT-BE"],
+#     4: ["CERT-BE"],
+#     5: ["foo", "bar"],
+# }
+
+# Additional sharing groups. Provisioned on all instances.
+# Note: central_node_org_name is always included in the SG
+additional_sharinggroups = None
+# additional_sharinggroups = {
+#     "Nato Countries": [
+#         f"{client_node_org_name_prefix}NATO",
+#         f"{client_node_org_name_prefix}ALB",
+#         f"{client_node_org_name_prefix}BEL",
+#         f"{client_node_org_name_prefix}BGR",
+#     ],
+#     "7NNNs": [
+#         f"{client_node_org_name_prefix}XYZ",
+#     ],
+#     "Partners": [
+#         f"{client_node_org_name_prefix}BGR",
+#     ],
+#     "National Civilian Organizations": [
+#         f"{client_node_org_name_prefix}BGR",
+#         f"{client_node_org_name_prefix}XYZ",
+#     ],
+# }
